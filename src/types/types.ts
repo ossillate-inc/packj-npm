099e4422bfb0132561a5a0a21036e9ba32a2fe0e
KhoiUna <thorwaitson@gmail.com>
2023-04-06 14:34:24 -0500

    modified:   src/types/types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,3 +7,10 @@ export type AuthCode = {
   code: string;
   state: string;
 };
+
+export type AuthToken = {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  refresh_token: string;
+};
