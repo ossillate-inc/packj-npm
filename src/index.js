00539d81711f901b84d69ce2af4ce07437aae93f
KhoiUna <thorwaitson@gmail.com>
2023-04-06 15:31:21 -0500

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+import auditPackage from "./lib/auditPackage.js";
 import getAuthCode from "./lib/getAuthCode.js";
 import getAuthToken from "./lib/getAuthToken.js";
 import setupSession from "./lib/setupSession.js";
 
-// 1. set up session -> gives you ID
+// set up session -> gives you ID
 const clientID = await setupSession();
 if (!clientID) process.exit(1);
 
-// 2. get auth code
+// get auth code
 const authCode = await getAuthCode(clientID);
 if (!authCode) process.exit(1);
 
-// 3. get auth token
-const authToken = await getAuthToken(clientID, authCode);
-console.log(authToken);
-if (!authToken) process.exit(1);
+// get auth token
+const { access_token } = await getAuthToken(clientID, authCode);
+if (!access_token) process.exit(1);
+
+// audit package
+const response = await auditPackage("npm", "react", access_token);
+console.log(response);
