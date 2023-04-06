414ae81d2aada3f4944525bbb84213b3152334a5
KhoiUna <thorwaitson@gmail.com>
2023-04-06 16:07:46 -0500

    modified:   src/config/config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import os from "os";
 export const BASE_URL = "https://packj.dev";
 
 export const AUTH_ENDPOINT = "/oauth/authorize";
-export const SESSION_ENDPOINT = "/api/v1/cli_sessions";
+export const SESSION_ENDPOINT = "/api/v1/cli/npm/sessions";
 export const TOKEN_ENDPOINT = "/oauth/token";
 export const REDIRECT_ENDPOINT = "/oauth/code/callback";
 export const AUDIT_ENDPOINT = "/api/v1/audit";
