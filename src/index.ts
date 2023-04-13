35796f72c63aed7bd73695c8ef5efe069a9567c8
KhoiUna <thorwaitson@gmail.com>
2023-04-12 20:30:19 -0500

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,93 @@
 import chalk from "chalk";
-
 import auditPackage from "./audit/main.js";
 import getAuthCode from "./auth/code.js";
 import getAuthToken from "./auth/token.js";
 import setupSession from "./auth/session.js";
+import { readFileSync, writeFileSync } from "node:fs";
 
-// set up session -> gives you ID
-const clientID = await setupSession();
-if (!clientID) process.exit(1);
+const args = process.argv;
 
-// get auth code
-const authCode = await getAuthCode(clientID);
-if (!authCode) process.exit(1);
+if (args.length < 3) {
+  const usageText = `
+    usage: main [options] args
 
-// get auth token
-const access_token = await getAuthToken(clientID, authCode);
-if (!access_token) process.exit(1);
+    Packj flags malicious/risky open-source packages
 
-// audit package
-const response = await auditPackage("npm", "react", "latest", access_token);
-console.log(response);
-console.log(chalk.greenBright("Audit successfully!"));
-process.exit();
+    args:
+      auth                Authenticate user with the server.
+      audit               Audit packages for malware/risky attributes
+  `;
+  console.log(usageText);
+  process.exit();
+}
+
+const option = args[2] as "auth" | "audit";
+
+if (option === "auth") {
+  const data = await setupSession();
+  if (!data) process.exit(1);
+
+  const { id: clientID, auth_url } = data;
+
+  // get auth code
+  const authCode = await getAuthCode(clientID);
+  if (!authCode) process.exit(1);
+
+  // get auth token
+  const authTokenData = await getAuthToken(clientID, authCode);
+  if (!authTokenData) process.exit(1);
+
+  // TODO: Write data to ~/.packj.creds
+  const content = {
+    auth_url,
+    code: authCode,
+    id: clientID,
+    token: {
+      ...authTokenData,
+      expires: "<expire>", // TODO: change
+    },
+  };
+  writeFileSync("./.packj.creds", JSON.stringify(content), { flag: "w" });
+  process.exit();
+}
+
+if (option === "audit") {
+  if (args.length < 4) {
+    const usageText = `
+    usage: main audit PACKAGES
+
+    args: 
+      PACKAGES: Audit packages (e.g., axios:1.3.5)
+  `;
+    console.log(usageText);
+    process.exit();
+  }
+
+  if (args[3].split(":").length < 2) {
+    console.log(chalk.red("Error: Invalid package"));
+    process.exit(1);
+  }
+
+  const [packageName, packageVersion] = args[3].split(":");
+
+  const fileText = readFileSync("./.packj.creds", "utf8");
+  const accessToken = JSON.parse(fileText).token.access_token; // TODO: read from ~/.packj.creds
+  if (!accessToken) {
+    console.log(chalk.red("Error: Invalid client ID"));
+    process.exit(1);
+  }
+
+  // Audit package
+  const response = await auditPackage(
+    "npm",
+    packageName,
+    packageVersion,
+    accessToken
+  );
+  if (!response) process.exit(1);
+
+  console.log(response);
+  console.log();
+  console.log(chalk.greenBright("Audit successfully!"));
+  process.exit();
+}
