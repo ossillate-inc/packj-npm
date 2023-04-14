98e1146faa873d8cfc5fe5ded7b7352d530d0199
KhoiUna <thorwaitson@gmail.com>
2023-04-14 18:46:23 -0500

    update: write .packj.creds to homedir

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,8 @@ export const TOKEN_ENDPOINT = "/oauth/token";
 export const REDIRECT_ENDPOINT = "/oauth/code/callback";
 export const AUDIT_ENDPOINT = "/api/v1/audit";
 
+export const CREDS_FILE_PATH = os.homedir() + "/.packj.creds";
+
 export const PACKJ_VERSION = "0.15";
 
 export const HOSTNAME = os.hostname();
