1301cd2cf1acf96a36f1638b3548ae15d84ee348
KhoiUna <thorwaitson@gmail.com>
2023-04-24 16:58:19 -0500

    modified:   src/commands/audit.ts

--- src/commands/audit.ts
+++ src/commands/audit.ts
@@ -1,4 +1,4 @@
-import { Args, Command, Flags } from '@oclif/core'
+import { Args, Command, Flags, ux } from '@oclif/core'
 import auditPackage from '../audit/main'
 import { existsSync, readFileSync } from 'fs'
 import { CREDS_FILE_PATH } from '../config'
@@ -12,31 +12,27 @@ export default class Audit extends Command {
   ]
 
   static flags = {
-    file: Flags.boolean({ char: 'f' }),
+    file: Flags.string({
+      char: 'f',
+      name: 'Package file',
+      description: 'Package name for auditing. Eg: npm:package.json, pypi:requirements.txt, etc.',
+      exists: true
+    }),
   }
 
   static args = {
     package: Args.string({
-      required: true
+      name: 'Package name',
+      description: 'Package name for auditing. Eg: npm:axios:1.3.5, pypi:requests:1.0, etc.'
     })
   }
 
-  public async run(): Promise<void> {
-    const { args, flags } = await this.parse(Audit)
-
-    if (flags.file) return
-
-    const [packageManager, packageName, packageVersion] = args.package.split(":");
-
-    if (!packageManager || !packageName || !packageVersion) this.error('Wrong package format', {
+  private async auditPackage(packageManager: string,
+    packageName: string,
+    packageVersion: string) {
+    if (!existsSync(CREDS_FILE_PATH)) this.error("User not authenticated. Please run 'auth' before 'audit'", {
       exit: 1
     })
-
-    if (!existsSync(CREDS_FILE_PATH)) {
-      this.error("User not authenticated. Please run 'auth' before 'audit'", {
-        exit: 1
-      })
-    }
 
     const fileText = readFileSync(CREDS_FILE_PATH, "utf8");
     const accessToken = JSON.parse(fileText).token.access_token;
@@ -54,8 +50,79 @@ export default class Audit extends Command {
     if (error) this.error(error.message + " Please run 'auth' again.", {
       exit: 1
     })
+    if (!success) this.error('Error auditing package.', {
+      exit: 1
+    })
 
-    console.log(success);
-    this.log("Audit successfully!")
+    return success
+  }
+
+  public async run(): Promise<void> {
+    const { args, flags } = await this.parse(Audit)
+
+    // Audit file package
+    if (flags.file) {
+      const [packageManager, filePath] = flags.file.split(':')
+
+      if (!packageManager) this.error('Invalid package file format.', {
+        exit: 1
+      })
+
+      if (packageManager === 'pypi') return
+
+      const packageFile = readFileSync(filePath, 'utf8')
+      const dependencyObject = JSON.parse(packageFile).dependencies
+      Object.entries(dependencyObject).forEach(async ([packageName, packageVersion], index) => {
+        const response = await this.auditPackage(packageManager, packageName, packageVersion as string)
+
+        ux.table([response], {
+          package_manager: {},
+          package_name: {
+            get: row => row.packages[0].name,
+            minWidth: 25
+          },
+          version: {
+            get: row => row.packages[0].version
+          },
+          risks: {
+            get: row => row.packages[0].risks
+          },
+          url: {},
+        }, {
+          "no-header": index > 0
+        })
+      })
+      return
+    }
+
+    // Audit packagse
+    if (!flags.file) {
+      const packageInput = args.package
+      if (!packageInput) this.error('Package is not defined!', {
+        exit: 1
+      })
+
+      const [packageManager, packageName, packageVersion] = packageInput.split(":");
+      if (!packageManager || !packageName || !packageVersion) this.error('Wrong package format', {
+        exit: 1
+      })
+
+      const response = await this.auditPackage(packageManager, packageName, packageVersion)
+
+      ux.table([response], {
+        package_manager: {},
+        package_name: {
+          get: row => row.packages[0].name,
+          minWidth: 25
+        },
+        version: {
+          get: row => row.packages[0].version
+        },
+        risks: {
+          get: row => row.packages[0].risks
+        },
+        url: {},
+      })
+    }
   }
 }
