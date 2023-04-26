8b5ccdf21d0a10198d5fa533798b5915412bbbdc
KhoiUna <thorwaitson@gmail.com>
2023-04-26 14:41:16 -0500

    add dependency file auditing

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,27 @@ import getAuthCode from "./auth/code.js";
 import getAuthToken from "./auth/token.js";
 import setupSession from "./auth/session.js";
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
+import * as readline from 'node:readline'
+import * as fs from 'node:fs'
 import { CREDS_FILE_PATH } from "./config.js";
 
 const usageText = `
-  usage: main audit PACKAGES [PACKAGES ...]
+  usage: main COMMAND [-f] PACKAGE [DEPENDENCY_FILE]
+
+  COMMAND:
+    auth    Authenticate user with the server.
+    audit   Audit packages for malware/risky attributes
+
+  FLAGS:
+    -f DEPENDENCY_FILE: Audit dependency file
   
-  args: 
+  ARGS: 
     PACKAGES: Audit packages (e.g., npm:axios), optionally specific version (e.g., npm:axios:1.3.5) 
 `;
 
 const args = process.argv;
 
 if (args.length < 3) {
-  const usageText = `
-    usage: main [options] args
-
-    Packj flags malicious/risky open-source packages
-
-    args:
-      auth                Authenticate user with the server.
-      audit               Audit packages for malware/risky attributes
-  `;
   console.log(usageText);
   process.exit();
 }
@@ -40,7 +40,7 @@ if (option === "auth") {
     const expiresAt = new Date(token.expires).valueOf();
 
     if (expiresAt < Date.now()) {
-      throw Error("Tokens expired!");
+      throw "Tokens expired! Reauthenticate..."
     }
     console.log("Already authenticated. Nothing to do.");
     process.exit(1);
@@ -50,7 +50,7 @@ if (option === "auth") {
       // nothing to do, fall through
     } else if (err.code !== "ENOENT") {
     } else {
-      console.log(chalk.red("Error loading credentials. Ignoring."));
+      console.error(chalk.red("Error: Error loading credentials. Ignoring."));
     }
   }
 
@@ -81,7 +81,7 @@ if (option === "auth") {
     writeFileSync(CREDS_FILE_PATH, JSON.stringify(content), { flag: "w" });
     console.log("Successfully authenticated (account activated).");
   } catch (err) {
-    console.log(chalk.red("Failed to authenticate: @{err}"));
+    console.error(chalk.red("Error: Failed to authenticate: @{err}"));
   }
   process.exit();
 }
@@ -92,22 +92,95 @@ if (option === "audit") {
     process.exit();
   }
 
-  const [packageManager, packageName, packageVersion] = args[3].split(":");
-  if (!packageManager || !packageName) {
-    console.log(chalk.red("Error: Invalid input"));
-    console.log(usageText);
-    process.exit(1);
-  }
-
   if (!existsSync(CREDS_FILE_PATH)) {
-    console.log(chalk.red("User not authenticated. Run 'auth' before 'audit'"));
+    console.error(chalk.red("Error: User not authenticated. Run 'auth' before 'audit'"));
     process.exit(1);
   }
 
   const fileText = readFileSync(CREDS_FILE_PATH, "utf8");
   const accessToken = JSON.parse(fileText).token.access_token;
   if (!accessToken) {
-    console.log(chalk.red("Invalid client ID"));
+    console.error(chalk.red("Error: Invalid client ID"));
+    process.exit(1);
+  }
+
+  const flag = args[3]
+
+  if (flag === '-f') {
+    const dependencyFile = args[4]
+
+    if (!dependencyFile || dependencyFile.split(':').length > 2) {
+      console.log(usageText);
+      process.exit();
+    }
+
+    const [packageManager, filePath] = dependencyFile.split(':')
+    if (!packageManager || !filePath) {
+      console.log(chalk.red('Error: Invalid package file format.'));
+      process.exit(1)
+    }
+
+    if (!existsSync(filePath)) {
+      console.error(chalk.red("Error: Invalid file path"));
+      process.exit(1);
+    }
+
+    if (packageManager === 'pypi') {
+      const readInterface = readline.createInterface({
+        input: fs.createReadStream(filePath),
+      });
+
+      const output = []
+      for await (const line of readInterface) {
+        const [packageName, packageVersion] = line.split('==')
+        const response = await auditPackage(packageManager, packageName, packageVersion, accessToken)
+        if (!response) {
+          console.error(chalk.red('Error: Error auditing dependency file'));
+          process.exit(1)
+        }
+
+        const formattedResponse = {
+          package_manager: response.package_manager,
+          package_name: response.packages[0].name,
+          package_version: response.packages[0].version,
+          risks: response.packages[0].risks,
+          url: response.url
+        }
+        output.push(formattedResponse)
+      }
+      console.table(output, Object.keys(output[0]))
+      process.exit()
+    }
+
+    const packageFile = readFileSync(filePath, 'utf8')
+    const dependencyObject = JSON.parse(packageFile).dependencies
+
+    const output = []
+    for (const [packageName, packageVersion] of Object.entries(dependencyObject)) {
+      const response = await auditPackage(packageManager, packageName, packageVersion as string, accessToken)
+      if (!response) {
+        console.error(chalk.red('Error: Error auditing package.'));
+        process.exit(1)
+      }
+
+      const formattedResponse = {
+        package_manager: response.package_manager,
+        package_name: response.packages[0].name,
+        package_version: response.packages[0].version,
+        risks: response.packages[0].risks,
+        url: response.url
+      }
+      output.push(formattedResponse)
+    }
+
+    console.table(output, Object.keys(output[0]))
+    process.exit()
+  }
+
+  const [packageManager, packageName, packageVersion] = args[3].split(":");
+  if (!packageManager || !packageName) {
+    console.error(chalk.red("Error: Invalid input"));
+    console.log(usageText);
     process.exit(1);
   }
 
@@ -120,8 +193,13 @@ if (option === "audit") {
   );
   if (!response) process.exit(1);
 
-  console.log(response);
-  console.log();
-  console.log(chalk.greenBright("Audit successfully!"));
+  const formattedResponse = {
+    package_manager: response.package_manager,
+    package_name: response.packages[0].name,
+    package_version: response.packages[0].version,
+    risks: response.packages[0].risks,
+    url: response.url
+  }
+  console.table([formattedResponse], Object.keys(formattedResponse))
   process.exit();
 }
